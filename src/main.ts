1280aec956add7007bb66da1cffb2ef6c5d9a158
匡一珉 <kuangyimin@xdf.cn>
2022-06-30 00:35:45 +0800

    add

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from 'virtual:generated-pages'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
+
+const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
+
